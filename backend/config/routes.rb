ceb9eaa7dd4ec86e8aeb97589ab80657394a5273
CAIOS0922 <caios.system@gmail.com>
2023-05-07 19:16:49 +0900

    create article_browse_history table

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,12 +1,12 @@
-Rails.application.routes.draw do
-  namespace :api do
-    resources :articles, only: :index do
-      collection do
-        resources :markdown, only: %i[index show], module: :articles
-        resources :qiita, only: :index, module: :articles
-        resources :zenn, only: :index, module: :articles
-        post :browse, to: "articles#browse"
+    Rails.application.routes.draw do
+      namespace :api do
+        resources :articles, only: :index do
+          collection do
+            resources :markdown, only: %i[index show], module: :articles
+            resources :qiita, only: :index, module: :articles
+            resources :zenn, only: :index, module: :articles
+            post :browse, to: "articles#browse"
+          end
+        end
       end
     end
-  end
-end

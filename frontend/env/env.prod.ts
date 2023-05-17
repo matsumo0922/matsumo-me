34c31352f67fdd74cee37d5243cd0794bc96c33b
daichi-matsumoto <caios.system@gmail.com>
2023-05-17 23:44:05 +0900

    change production api endpoint

--- frontend/env/env.prod.ts
+++ frontend/env/env.prod.ts
@@ -1,4 +1,4 @@
 module.exports = {
-    NEXT_PUBLIC_API_ENDPOINT: "https://api.matsumo.me/api",
-    NEXT_PUBLIC_API_CROS_ENDPOINT: "https://api.matsumo.me/api",
+    NEXT_PUBLIC_API_ENDPOINT: "https://matsumo-me-api.fly.dev/api",
+    NEXT_PUBLIC_API_CROS_ENDPOINT: "https://matsumo-me-api.fly.dev/api",
 }

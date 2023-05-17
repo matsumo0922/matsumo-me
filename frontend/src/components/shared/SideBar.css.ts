5448e4bb595d3c52dfdfd6df2e90a8000f86afaa
daichi-matsumoto <caios.system@gmail.com>
2023-05-18 00:17:51 +0900

    add responsive logic to sidebar

--- frontend/src/components/shared/SideBar.css.ts
+++ frontend/src/components/shared/SideBar.css.ts
@@ -1,13 +1,20 @@
 import {style} from "@vanilla-extract/css";
 import {appContainer, center} from "@/styles/system.css";
 import {resources} from "@/styles/resources.css";
+import {mediaQueries} from "@/styles/mediaQueries";
 
 export const sideBarStyle = style({
    maxWidth: resources.sideBarMaxWidth,
-   display: "flex",
+   display: "none",
    flexFlow: "column",
    alignItems: "flex-start",
    gap: resources.spacing[6],
    paddingTop: resources.spacing[2],
    paddingLeft: resources.spacing[8],
+
+   "@media": {
+      [mediaQueries.largerThan("md")]: {
+         display: "flex",
+      },
+   },
 });
